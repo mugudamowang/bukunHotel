56218eaa8a565d7918d02a89f0f97ea87797a2f6
mugu <1175233520@qq.com>
2020-04-21 21:39:49 +0800

    this is bukunHotel v1.1.0

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,25 +4,48 @@ const app = getApp()
 
 Page({
   data: {
+    // viewheight: 0,
+    // viewwidth: 0,
+
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    vertical: false,
+    duration: 500,
     currentTab: 0,
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
+    // wx.getSystemInfo({
+    //   success: (res) => {
+    //     console.log("fuck")
+    //     this.setData({
+    //       viewwidth: res.windowWidth,
+    //       viewheight: res.windowHeight
+    //     })
+    //     console.log(this.viewheight)
+    //     console.log("you")
+    //   },
+    // })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -44,7 +67,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({

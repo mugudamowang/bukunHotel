463f4cc55a383670ba954db4b4cbe94f7cccaa32
mugu <1175233520@qq.com>
2020-04-25 22:40:33 +0800

    add mostly CSS of userPost.wxml

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-    // viewheight: 0,
-    // viewwidth: 0,
+    // 当前是静态数据-----------------------------------------
+    datalist:[
+      {
+        id : "0233",
+        post : "谁给你写诗?"
+      },
+      {
+        id : "0404",
+        post : "海星如何睡觉?"
+      },
+      {
+        id : "0618",
+        post : "hhh"
+      },
+      {
+        id : "1010",
+        post : "covid19给爷爬"
+      },
+      {
+        id : "8856",
+        post : "高三加油"
+      },
+      {
+        id : "1010",
+        post : "covid19给爷爬"
+      },
+      {
+        id : "8856",
+        post : "高三加油"
+      }
+    ],
+    //----------------------------------------------
 
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     vertical: false,

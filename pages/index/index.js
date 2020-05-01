52740e31b2bc141c3b372f4cc632116f4688ad82
mugu <1175233520@qq.com>
2020-05-01 15:14:47 +0800

    complete all basic page

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,39 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    // 当前是静态数据-----------------------------------------
-    datalist:[
-      {
-        id : "0233",
-        post : "谁给你写诗?"
-      },
-      {
-        id : "0404",
-        post : "海星如何睡觉?"
-      },
-      {
-        id : "0618",
-        post : "hhh"
-      },
-      {
-        id : "1010",
-        post : "covid19给爷爬"
-      },
-      {
-        id : "8856",
-        post : "高三加油"
-      },
-      {
-        id : "1010",
-        post : "covid19给爷爬"
-      },
-      {
-        id : "8856",
-        post : "高三加油"
-      }
-    ],
-    //----------------------------------------------
-
+    datalist: app.globalData.datalist,
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     vertical: false,
     duration: 500,
@@ -45,6 +13,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
 
   durationChange(e) {

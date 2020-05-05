77c2e3eca2239358c824724ce2c9314f94b56a5d
mugu <1175233520@qq.com>
2020-05-05 22:38:17 +0800

    ready to merge

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ## 每次 pull push 完 master 要看↓↓↓
-### 2020/... XXX : ..........往接着上写,这样打开github下面就直接可以看这个readme.md文件
+### 2020/5/4 lyq
+- 在app.json的首行添加了cloud:true
+- 在app.js的onload函数中对云函数进行初始化init
+- 在project.config.json中新增了cloudfunctionRoot来指定一个文件夹来新建云函数，一开始按右键新建并没有云函数这个选项，在这里指定了路径之后就
+右键新建就出现了云函数
+- 我的云数据库的环境id命名为bukunhotel，还没有了解如何导出数据库文件的相关信息和环境id的冲突问题
+- 在进行数据渲染时踩的坑是部署过云函数之后，又修改了云函数没有再上传并部署导致一直找不到问题
+- 用的是less文件编写样式，注释都在less文件中而不在wxss文件中
 ### 2020/4/22 mugu:
 - 调整了index页面设置,其中禁用页面滚动,使topNavBar的导航不会滚动
 - 初步userPost调整样式
@@ -10,11 +17,4 @@
 - 命名最好统一下, 可以参考bukunHotel.png
 - loading要是嫌加载慢的话,开发2的时候可以在app.json里把位置往后排
 - 这样子编辑文档,主要是添加一些说明文字, 用md写
-### 2020/5/4 lyq
--在app.json的首行添加了cloud:true
--在app.js的onload函数中对云函数进行初始化init
--在project.config.json中新增了cloudfunctionRoot来指定一个文件夹来新建云函数，一开始按右键新建并没有云函数这个选项，在这里指定了路径之后就
-右键新建就出现了云函数
--我的云数据库的环境id命名为bukunhotel，还没有了解如何导出数据库文件的相关信息和环境id的冲突问题
--在进行数据渲染时踩的坑是部署过云函数之后，又修改了云函数没有再上传并部署导致一直找不到问题
--用的是less文件编写样式，注释都在less文件中而不在wxss文件中
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-<<<<<<< HEAD
-    // viewheight: 0,
-    // viewwidth: 0,
 
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    vertical: false,
-    duration: 500,
-=======
     //用户信息
->>>>>>> lyq
     currentTab: 0,
     motto: 'Hello World',
     userInfo: {},
@@ -42,74 +34,7 @@ Page({
     })
   },
   onLoad: function () {
-<<<<<<< HEAD
-    // wx.getSystemInfo({
-    //   success: (res) => {
-    //     console.log("fuck")
-    //     this.setData({
-    //       viewwidth: res.windowWidth,
-    //       viewheight: res.windowHeight
-    //     })
-    //     console.log(this.viewheight)
-    //     console.log("you")
-    //   },
-    // })
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-=======
     this.getList();
-
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
->>>>>>> lyq
   },
   getUserInfo: function (e) {
     console.log(e)
@@ -118,10 +43,6 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-<<<<<<< HEAD
-  }
-})
-=======
   },
   getList() {
     let that = this;
@@ -173,4 +94,3 @@ Page({
     this.getList();
 },
 })
->>>>>>> lyq

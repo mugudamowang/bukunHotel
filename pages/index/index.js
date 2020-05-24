0c63cc4c049286f1ff5394bbbf234a4d93c998d7
zatylrh <2764472435@qq.com>
2020-05-24 18:40:23 +0800

    暂停跳转第一步

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,9 +32,9 @@ Page({
       currentTime: '0:00',
       currentSeconds: 0,
       duration: 0,
-      durationFormat:'0:00',
+      durationFormat: '0:00',
       totalTime: '0:00',
-      totalSeconds:0,
+      totalSeconds: 0,
       title: '轻声细雨',
       singer: '',
       coverImgUrl: 'cloud://bukunhotel-rxvhp.6275-bukunhotel-rxvhp-1302029966/picture/细雨落叶.jpg'
@@ -158,55 +158,81 @@ Page({
     audioCtx.epname = music.name
     audioCtx.singer = music.name
 
-    // this.audioCtx.src=music.music_url
+    audioCtx.src=music.music_url
     // audioCtx.src = "http://183.240.120.29/amobile.music.tc.qq.com/C400001KQ3zX0N2rVR.m4a?guid=185019120&amp;vkey=D8F7BFF89ECE89AC5D8DCCF7173413FEEC9F4D3BE51BC96DED7B66B6B50B9EB0D64F58987760909B62F71503AA4C2C06D8F1B7A93BEC0D56&amp;uin=0&amp;fromtag=66"
-    audioCtx.src = "http://183.240.120.18/amobile.music.tc.qq.com/C400002I3Nwa4f9xqA.m4a?guid=4680889107&amp;vkey=977534C2CDF8CB0B6EF5698D62EA45474AE69C7B31FE80E69E06EB3DD5F349FA2905A4DD5F2FD441352D3BDCD4C76E994709613D7F605F6E&amp;uin=115&amp;fromtag=66"
-    // audioCtx.src="http://183.240.120.29/amobile.music.tc.qq.com/C400000trBUk47EpIL.m4a?guid=4680889107&amp;vkey=481BF7496B76B8B8679384F83453766C5FFF3F996503230E81FDC5BD5D2BB81B09A7DE273C401CD31A3CB9AF446919B8D25F20DF054C5AEE&amp;uin=115&amp;fromtag=66"
+    // audioCtx.src = "http://183.240.120.18/amobile.music.tc.qq.com/C400002I3Nwa4f9xqA.m4a?guid=4680889107&amp;vkey=977534C2CDF8CB0B6EF5698D62EA45474AE69C7B31FE80E69E06EB3DD5F349FA2905A4DD5F2FD441352D3BDCD4C76E994709613D7F605F6E&amp;uin=115&amp;fromtag=66" 贝贝
+    // audioCtx.src="http://183.240.120.29/amobile.music.tc.qq.com/C400000trBUk47EpIL.m4a?guid=4680889107&amp;vkey=481BF7496B76B8B8679384F83453766C5FFF3F996503230E81FDC5BD5D2BB81B09A7DE273C401CD31A3CB9AF446919B8D25F20DF054C5AEE&amp;uin=115&amp;fromtag=66" 光
     // console.log(this.audioCtx)
-    
+
     // 如果是第一次播放，则初始化totalTime为0:00
-if(this.data.loops==0){
-  this.setData({
-    playIndex: index,
-    'play.title': music.name,
-    'play.coverImgUrl': music.pic_url,
-    'play.currentTime': '0:00',
-    'play.totalTime':'0:00',
-    'play.totalSeconds':0
+    if (this.data.loops == 0) {
+      this.setData({
+        playIndex: index,
+        'play.title': music.name,
+        'play.coverImgUrl': music.pic_url,
+        'play.currentTime': '0:00',
+        'play.totalTime': '0:00',
+        'play.totalSeconds': 0
 
-  })
-}
-// 若不是第一次播放了，则让totalTime初始化为其总共时长 ，让其累加
-else if(this.data.loops==1){
-  this.setData({
-    playIndex: index,
-    'play.title': music.name,
-    'play.coverImgUrl': music.pic_url,
-    
-    'play.currentTime': '0:00',
-    'play.totalTime':this.data.play.durationFormat,
-    'play.totalSeconds':this.data.play.duration
+      })
+    }
+    // 若不是第一次播放了，则让totalTime初始化为其总共时长 ，让其累加
+    else if(this.data.loops==1){
+      this.setData({
+        playIndex: index,
+        'play.title': music.name,
+        'play.coverImgUrl': music.pic_url,
 
-  })
-}
-else {
-  this.setData({
-    playIndex: index,
-    'play.title': music.name,
-    'play.coverImgUrl': music.pic_url,
-    
-    'play.currentTime': '0:00',
-    'play.totalTime':this.data.play.totalTime,
-    'play.totalSeconds':this.data.play.totalSeconds
+        'play.currentTime': '0:00',
+        'play.totalTime':this.data.play.durationFormat,
+        'play.totalSeconds':this.data.play.duration
 
-  })
-}
-    
+      })
+    }
+    else {
+      this.setData({
+        playIndex: index,
+        'play.title': music.name,
+        'play.coverImgUrl': music.pic_url,
+
+        'play.currentTime': '0:00',
+        'play.totalTime':this.data.play.totalTime,
+        'play.totalSeconds':this.data.play.totalSeconds
+
+      })
+    }
+
+    if (this.data.loops > 0) {
+      if (this.data.play.totalSeconds != 0) {
+        this.setData({
+          playIndex: index,
+          'play.title': music.name,
+          'play.coverImgUrl': music.pic_url,
+
+          'play.currentTime': '0:00',
+          'play.totalTime': this.data.play.totalTime,
+          'play.totalSeconds': this.data.play.totalSeconds
+
+        })
+      }
+      else {
+        this.setData({
+          playIndex: index,
+          'play.title': music.name,
+          'play.coverImgUrl': music.pic_url,
+
+          'play.currentTime': '0:00',
+          'play.totalTime': this.data.play.durationFormat,
+          'play.totalSeconds': this.data.play.duration
+
+        })
+      }
+    }
 
   },
-  // 播放功能，对于播放条那里直接触发背景音乐实例的play，不用setMusic；对于图片点击就要用change来setMusic
+  // 用于图片点击的播放
   play: function () {
-    // audioCtx.play();
+
     let that = this
 
     // 转化为浮点类型，因为一开始定义是string类型
@@ -220,32 +246,87 @@ else {
     // console.log('num is'+num,'viewing_time is'+viewing_time)
 
 
-    let currentSeconds=this.data.play.currentSeconds
-    let duration=this.data.play.duration
+    let currentSeconds = this.data.play.currentSeconds
+    let duration = this.data.play.duration
     // 当缓冲的时间不为0且这首歌还未播放完
-    if (viewing_time != 0&&currentSeconds<duration) {
+
+    if (viewing_time != 0 && currentSeconds < duration) {
       // 跳转到暂停时存储的时间
-      console.log('11111 我暂停之后又能跳转啦')
+      console.log('11111 我暂停之后又能回到以前啦 ')
       audioCtx.seek(viewing_time)
+      // 注释掉试试看的，好像没什么关系
+      // wx.playBackgroundAudio()
+      // that.setData({
+      //    is_play: true,
+
+      //  })
+
+
+
+      //   // let that=this
+      //   //   // // 背景音乐播放完毕    // 播放结束时要做的事情是啥？？？？？？？
+
+
+      //   audioCtx.onEnded(() => {
+      //    console.log('我已经播放完啦 我要再次播放')
+      //    // 放完后设置播放状态为不播放？？？？？？还是播放完后要更新一下时间
+      //    let loops=that.data.loops+1
+      //   that.setData({
+      //    loops:loops,
+      //    // is_play:false
+      //    })
+      //    // 加上这句话在模拟器才能中正常
+      //    that.updateViewTime()
+
+      //    that.setMusic(that.data.playIndex)
+      //    that.play()
+
+
+      //  //  that.change(this.data.playIndex)
+      //    console.log('又可以重新播放啦')
+      //  })
       that.currentTimeChange()
+
     }
     else {
       that.currentTimeChange()
-      console.log('2222 我暂停之后又不能跳转')
-    }
 
-    // })
+    }
 
 
     // 正式开始播放
     wx.playBackgroundAudio()
-   that.setData({
+    that.setData({
       is_play: true,
-     
+
+    })
+// 一开始是没有加的，后来为了解决问题加上去的，但也不知道有没有用
+    //   // // 背景音乐播放完毕    // 播放结束时要做的事情是啥？？？？？？？
+    audioCtx.onEnded(() => {
+      console.log('我已经播放完啦 我要再次播放')
+      // 放完后设置播放状态为不播放？？？？？？还是播放完后要更新一下时间
+      let loops = this.data.loops + 1
+      that.setData({
+        loops: loops,
+        //  is_play:false
+      })
+      // 加上这句话在模拟器才能中正常
+      that.updateViewTime()
+
+      that.setMusic(that.data.playIndex)
+      that.play()
+
+
+      //  that.change(this.data.playIndex)
+      console.log('又可以重新播放啦')
     })
 
-   
 
+  },
+  // 用于播放条的播放
+  play2: function () {
+    this.setMusic(this.data.playIndex)
+    this.play()
 
   },
   // 暂停功能
@@ -255,7 +336,8 @@ else {
     this.setData({
       is_play: false
     })
-    this.updateViewTime()
+    // this.updateViewTime()
+    this.data.viewing_time = this.data.play.currentSeconds
   },
   // 换歌
 
@@ -266,15 +348,20 @@ else {
     // 当点击的是同一首并且这首正在播放，则暂停
     if (this.data.playIndex == current && this.data.is_play == true) {
       this.pause()
-      console.log("这一首暂停！")
+      console.log("这一首暂停！ ")
       // 结束循环
       return;
 
     }
     //  // 当点击的是同一首并且这首正在暂停，则接着上一次的时间播放
     if (this.data.playIndex == current && this.data.is_play == false) {
+      //  乱加的试试看，但这是解决暂停后无法循环播放的关键
+      this.setMusic(this.data.playIndex)
       this.play()
-      console.log("又继续放这一首啦！")
+
+      console.log("又继续放这一首啦！ ")
+
+
     }
     //  换成其他歌曲
     else {
@@ -292,69 +379,73 @@ else {
   currentTimeChange: function () {
     // 可能存在的bug？？
     // let self = this;
-    let that=this
- //   // // 背景音乐播放完毕    // 播放结束时要做的事情是啥？？？？？？？
- 
+    let that = this
+    //   // // 背景音乐播放完毕    // 播放结束时要做的事情是啥？？？？？？？
 
- audioCtx.onEnded(() => {
-  console.log('我已经播放完啦 我要再次播放')
-  // 放完后设置播放状态为不播放？？？？？？还是播放完后要更新一下时间
-  let loops=this.data.loops+1
- that.setData({
-  loops:loops,
-  // is_play:false
-  })
-  // that.updateViewTime()
 
-  that.setMusic(that.data.playIndex)
-  that.play()
-  
-  
-//  that.change(this.data.playIndex)
-  console.log('又可以重新播放啦')
-})
+    audioCtx.onEnded(() => {
+      console.log('我已经播放完啦 我要再次播放')
+      // 放完后设置播放状态为不播放？？？？？？还是播放完后要更新一下时间
+      let loops = this.data.loops + 1
+      that.setData({
+        loops: loops,
+        // is_play:false
+      })
+      // 加上这句话在模拟器才能中正常
+      that.updateViewTime()
+
+      that.setMusic(that.data.playIndex)
+      that.play()
+
+
+      //  that.change(this.data.playIndex)
+      console.log('又可以重新播放啦')
+    })
 
     // 自动更新播放进度
     audioCtx.onTimeUpdate(() => {
-      
-      
-      
-      
+
+
+
+
       // 如果从未完整播放完一遍
-      if(this.data.loops==0){
+      if (this.data.loops == 0) {
         console.log('我是第一次播放哦')
         this.setData({
-          'play.durationFormat':this.formatTime(audioCtx.duration),
+          'play.durationFormat': this.formatTime(audioCtx.duration),
           'play.duration': audioCtx.duration,
           'play.currentTime': this.formatTime(audioCtx.currentTime),
           'play.currentSeconds': audioCtx.currentTime,
-          'play.totalTime':this.formatTime(audioCtx.currentTime),
-'play.totalSeconds':audioCtx.currentTime
-         
-  
-  
+          'play.totalTime': this.formatTime(audioCtx.currentTime),
+          'play.totalSeconds': audioCtx.currentTime
+
+
+
         })
-   
+
       }
       // 如果已经完整播放过一遍了，总时间就要累加
-      else{
+      else {
 
         let total = (this.data.play.totalSeconds + audioCtx.currentTime)
-        console.log('不是第一次播放啦 已经播放了'+total)
-        this.setData({
+        console.log('不是第一次播放啦 已经播放了' + total)
+       
+          this.setData({
 
-          // 不太确定播放过的还要不要设置时长了？？？
-          'play.durationFormat':this.formatTime(audioCtx.duration),
-          'play.duration': audioCtx.duration,
-          'play.currentTime': this.formatTime(audioCtx.currentTime),
-          'play.currentSeconds': audioCtx.currentTime,
-          'play.totalTime': this.formatTime(total),
-          'play.totalSeconds':total
-        })
-      
+            // 不太确定播放过的还要不要设置时长了？？？
+            'play.durationFormat': this.formatTime(audioCtx.duration),
+            'play.duration': audioCtx.duration,
+            'play.currentTime': this.formatTime(audioCtx.currentTime),
+            'play.currentSeconds': audioCtx.currentTime,
+            'play.totalTime': this.formatTime(total),
+            'play.totalSeconds': total
+          })
+       
+
+
       }
-     
-      
+
+
 
     });
   }
